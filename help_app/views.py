285b73f62d2c6fe98e4d9cb38bcb84d738dae7e6
Harsh Singh <hisingh1@gmail.com>
2012-06-27 11:33:10 -0500

    testing

---
+++ help_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render_to_response
+from django.template import Context, Template
+from django.template.loader import get_template
+from django.template import Context
+from django.views.decorators.csrf import csrf_exempt 
+from django.http import HttpResponse
+from django.core.cache import cache
+import json
+import random
+import logging
+import os
+
+def help(request):
+	return render_to_response("help_page.dtl")
+
+def help_itter(request, itter):
+	if int(itter) < 10 and int(itter) > 0:
+		return render_to_response("help_itter/help_page_"+str(itter)+".dtl")
