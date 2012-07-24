451d30d2461bb21dcb097aae3c1dd0606c0782a5
Harsh Singh <hsingh23@illinois.edu>
2012-07-24 17:00:27 -0500

    feat(api): add password-authed JSON/URL submission API
    Add /auth/ password gate (1-hour session), make-help-json and make-help-url
    form pages, and post-submit-json/url endpoints that store curated initial
    content in the Django cache. Rewrite get-initial to serve cached JSON or
    fetch from a stored URL, with JSONP callback validation and cache-control
    headers. Split the Website model into Website_JSON/Website_URL, enable
    XFrameOptions/ConditionalGet middleware and cache sessions, teach
    help_page_2.js to bootstrap from a ?q= parameter, and remove the
    experimental help_page_3-10/help_maker/slidedeck templates.

--- help_app/models.py
+++ help_app/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
-class Website(models.Model):
+class Website_JSON(models.Model):
     key_word = models.CharField(max_length=1000, db_index=True)
     initial_json = models.TextField()
+
+class Website_URL(models.Model):
+    key_word = models.CharField(max_length=1000, db_index=True)
+    initial_url = models.CharField(max_length=2000)
--- help_app/static/js/help_maker.js
+++ help_app/static/js/help_maker.js
@@ -1,22 +1,46 @@
-// $(".last").change(
-//     function (){
-//         $(this).after('<input type=text class="span3 more last">')
-//         .removeClass('last')
-//         .unbind();
-//         $(this).next().bind('change',changed);
-//     }
-// );
-// var changed = function () {
-//     $(this).after('<input type=text class="span3 more last">')
-//     .removeClass('last')
-//     .unbind()
-//     .next().bind('change',changed);
-// };
-$(".more.multi").change(
-    function (){
-        that = $(this);
-        that.before('<input type=text class="span3 more">')
-        .prev().val(that.val()).next().val("").focus();
-        $('input:text[value=\"\"]').not('.multi').not('.init').remove();
-    }
-);
+$(document).ready(function(){
+    $('.plus').click(do_stuff);
+    $(".more.multi").change(do_stuff_2);
+});
+
+function do_stuff(){
+    that = $(this);
+    that.before('<a class="btn btn-danger minus" href="#" onclick="$(this).prev().remove().end().remove(); return false;"><i class="icon-minus icon-white"></i></a>').before(that.prev().prev().clone())
+    .prev().prev().val(that.val()).next().val("").focus();
+}
+function do_stuff_2(){
+    that = $(this);
+    that.before(that.clone()).before('<a class="btn btn-danger minus" href="#" onclick="$(this).prev().remove().end().remove(); return false;"><i class="icon-minus icon-white"></i></a>');
+    that.val("").focus();
+}
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (this.name === 'csrfmiddlewaretoken' || this.value === ''){}
+        else if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
+$(function() {
+    $('form').submit(function() {
+        // console.log(JSON.stringify($('form').serializeObject()));
+        if ($("#keyword").val() === ""){
+            // missing - but required
+            $(".required").addClass("error");
+            return false;
+        }
+        $.post('/post-submit-json/', {'csrfmiddlewaretoken':$('input[name="csrfmiddlewaretoken"]').val(),'keyword':$("#initial_search_term").val(), 'json':String(JSON.stringify($('#submit_json').serializeObject()))} );
+        alert('ok');
+        return false;
+    });
+});
